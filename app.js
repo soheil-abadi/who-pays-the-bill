a54dc42ea6761805755ee5e15fcba40e6dcfb6e6
Pedram Kashani <kpedram@terafastnet.com>
2023-10-25 13:28:36 +0330

    session 1

--- app.js
+++ app.js
@@ -1,73 +1,51 @@
-let $ = document
-let body = document.querySelector('body');
-let mainSection = document.querySelector('.main-div');
-let startBtn = document.querySelector('.start-btn');
-let addtBtn = $.querySelector('.add');
-let names = $.querySelector('.names');
-let input = $.getElementById('nameValue');
-let warning = $.querySelector('.warning');
-let ran = $.querySelector('.random-name');
-let random = []
-focus()
-function focus() {
-    input.focus()
+const $ = document
+
+const addPersonToList = () => {
+    let warning = $.querySelector('.warning')
+    let names = $.querySelector('.names')
+    let personNameInput = $.querySelector('#person-name-input')
+    let random = []
+
+    const personName = personNameInput.value.trim()
+
+    if (!personName || personName.length < 3) {
+        warning.style.display = 'block'
+    } else {
+        let combine = document.createElement('li')
+        combine.innerHTML = personName
+        let hi = names.appendChild(combine)
+        personNameInput.value = ''
+        warning.style.display = 'none'
+        random.push(hi)
+        personNameInput.focus()
+    }
 }
 
-body.addEventListener('keydown', function (event) {
-
-
-    if (event.keyCode === 13) {
-        if (!input.value == "" && !input.value.startsWith(" ") && input.value.length>2) {
-            let combine = document.createElement('li')
-            combine.innerHTML = input.value.trim()
-            let hi = names.appendChild(combine)
-            input.value = ""
-            warning.style.display = 'none'
-            random.push(hi);
-            input.focus()
-
-        }
-        else {
-            warning.style.display = 'block'
-        }
-
-
-
-    }
-})
-
-addtBtn.addEventListener('click', function () {
-
-
-    if (!input.value == "") {
-        let combine = document.createElement('li')
-        combine.innerHTML = input.value
-        names.appendChild(combine)
-        input.value = ""
-        warning.style.display = 'none'
-        focus()
-        function focus() {
-            input.focus()
-        }
-
-    }
-    else {
-        warning.style.display = 'block'
+$.addEventListener('DOMContentLoaded', () => {
+    let body = document.querySelector('body')
+    let mainSection = document.querySelector('.main-div')
+    let startBtn = document.querySelector('.start-btn')
+    let addtBtn = $.querySelector('.add')
+    let personNameInput = $.querySelector('#person-name-input')
+    let ran = $.querySelector('.random-name')
+    focus()
+    function focus() {
+        personNameInput.focus()
     }
 
+    personNameInput.addEventListener('keydown', (e) => {
+        if (e.keyCode === 13) {
+            addPersonToList()
+        }
+    })
 
+    addtBtn.addEventListener('click', addPersonToList)
+
+    startBtn.addEventListener('click', function () {
+        let randomIndex = Math.floor(Math.random() * random.length)
+        let randomName = random[randomIndex]
+        ran.innerHTML = randomName.innerHTML + ' ' + 'you must pay the bill'
+        mainSection.style.display = 'none'
+        ran.style.visibility = 'visible'
+    })
 })
-startBtn.addEventListener('click', function () {
-    let randomIndex = Math.floor(Math.random() * random.length);
-    let randomName = random[randomIndex]
-    ran.innerHTML = randomName.innerHTML+ ' ' +'you must pay the bill'
-    mainSection.style.display = 'none'
-    ran.style.visibility = 'visible'
-
-})
-
-
-
-
-
-
