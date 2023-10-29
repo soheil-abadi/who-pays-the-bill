d71ec1d6b1a63d23af10cd287e9ab8520bedaefc
soheil-abadi <soheil.abadi1375@gmail.com>
2023-10-29 13:51:43 +0330

    improving style and methods

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+                    semi: true, // Use semicolons at the end of statements
+                    singleQuote: false, // Use double quotes for string literals
+                    tabWidth: 5, // Number of spaces per indentation level
+                    printWidth: 80, // Maximum line length before wrapping code
+                    useTabs: false, // Use spaces, not tabs, for indentation
+                    arrowParens: "always", // Include parentheses around a single parameter in arrow functions
+                    trailingComma: "none", // Do not add trailing commas in objects and arrays
+                    bracketSpacing: true, // Add spaces inside object literals
+                    jsxSingleQuote: false, // Use double quotes for JSX attributes
+                    jsxBracketSameLine: false // Place the closing bracket of JSX elements on a new line
+};
--- app.js
+++ app.js
@@ -1,54 +1,54 @@
 const $ = document;
 let random = [];
 const addPersonToList = () => {
-  let warning = $.querySelector(".warning");
-  let names = $.querySelector(".names");
-  let personNameInput = $.querySelector("#person-name-input");
-  const personName = personNameInput.value.trim();
+     let warning = $.querySelector(".warning");
+     let names = $.querySelector(".names");
+     let personNameInput = $.querySelector("#person-name-input");
+     const personName = personNameInput.value.trim();
 
-  if (!personName || personName.length < 3) {
-    warning.style.display = "block";
-  } else {
-    let combine = document.createElement("li");
-    combine.innerHTML = personName;
-    let hi = names.appendChild(combine);
-    personNameInput.value = "";
-    warning.style.display = "none";
-    random.push(hi);
+     if (!personName || personName.length < 3) {
+          warning.style.display = "block";
+     } else {
+          let combine = document.createElement("li");
+          combine.innerHTML = personName;
+          let hi = names.appendChild(combine);
+          personNameInput.value = "";
+          warning.style.display = "none";
+          random.push(hi);
 
-    personNameInput.focus();
-    combine.addEventListener("click", () => {
-      const index = random.indexOf(hi);
-      random.splice(index, 1);
-      combine.remove();
-    });
-  }
+          personNameInput.focus();
+          combine.addEventListener("click", () => {
+               const index = random.indexOf(hi);
+               random.splice(index, 1);
+               combine.remove();
+          });
+     }
 };
 
 $.addEventListener("DOMContentLoaded", () => {
-  let body = document.querySelector("body");
-  let mainSection = document.querySelector(".main-div");
-  let startBtn = document.querySelector(".start-btn");
-  let addtBtn = $.querySelector(".add");
-  let personNameInput = $.querySelector("#person-name-input");
-  let ran = $.querySelector(".random-name");
-  const focus = () => {
-    personNameInput.focus();
-  };
-  focus();
+     let body = document.querySelector("body");
+     let mainSection = document.querySelector(".main-div");
+     let startBtn = document.querySelector(".start-btn");
+     let addtBtn = $.querySelector(".add");
+     let personNameInput = $.querySelector("#person-name-input");
+     let ran = $.querySelector(".random-name");
+     const focus = () => {
+          personNameInput.focus();
+     };
+     focus();
 
-  personNameInput.addEventListener("keydown", (e) => {
-    if (e.keyCode === 13) {
-      addPersonToList();
-    }
-  });
+     personNameInput.addEventListener("keydown", (e) => {
+          if (e.keyCode === 13) {
+               addPersonToList();
+          }
+     });
 
-  addtBtn.addEventListener("click", addPersonToList);
-  startBtn.addEventListener("click", () => {
-    let randomIndex = Math.floor(Math.random() * random.length);
-    let randomName = random[randomIndex];
-    ran.innerHTML = randomName.innerHTML + " " + "you must pay the bill";
-    mainSection.style.display = "none";
-    ran.style.visibility = "visible";
-  });
+     addtBtn.addEventListener("click", addPersonToList);
+     startBtn.addEventListener("click", () => {
+          let randomIndex = Math.floor(Math.random() * random.length);
+          let randomName = random[randomIndex];
+          ran.innerHTML = randomName.innerHTML + " " + "you must pay the bill";
+          mainSection.style.display = "none";
+          ran.style.visibility = "visible";
+     });
 });
