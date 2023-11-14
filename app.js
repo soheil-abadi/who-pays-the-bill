3b203a4450bacb81650109ad46a4754f4bcd25bd
soheil-abadi <soheil.abadi1375@gmail.com>
2023-11-14 11:31:57 +0330

    make form arrangement with grid

--- app.js
+++ app.js
@@ -1,41 +1,43 @@
 const $ = document;
 let random = [];
-const addPersonToList = (event) => {
-     event.preventDefault();
-     let warning = $.querySelector(".warning");
-     let names = $.querySelector(".names");
-     let personNameInput = $.querySelector("#person-name-input");
-     const personName = personNameInput.value.trim();
-
-     if (!personName || personName.length < 3) {
-          warning.style.display = "block";
-     } else {
-          let creatLiPerson = $.createElement("li");
-          creatLiPerson.innerHTML = personName;
-          let personLiinput = names.appendChild(creatLiPerson);
-          personNameInput.value = "";
-          warning.style.display = "none";
-          random.push(personLiinput);
-          personNameInput.focus();
-          creatLiPerson.setAttribute("data-id", random.length - 1);
-          creatLiPerson.addEventListener("click", (e) => {
-               const index = e.target.dataset.id;
-               random.splice(index, 1);
-               creatLiPerson.remove();
-          });
-     }
-};
-
 $.addEventListener("DOMContentLoaded", () => {
      let mainSection = $.querySelector(".main-div");
      let startBtn = $.querySelector(".start-btn");
      let addtBtn = $.querySelector(".add");
      let personNameInput = $.querySelector("#person-name-input");
      let ran = $.querySelector(".random-name");
+     let form = $.querySelector("form");
+
      const focus = () => {
           personNameInput.focus();
      };
      focus();
+
+     const addPersonToList = (e) => {
+          e.preventDefault();
+          let warning = $.querySelector(".warning");
+          let names = $.querySelector(".names");
+          let personNameInput = $.querySelector("#person-name-input");
+          const personName = personNameInput.value.trim();
+          let formValidation = form.checkValidity();
+          if (!personName || personName.length < 3 || !formValidation) {
+               warning.style.display = "block";
+          } else {
+               let creatLiPerson = $.createElement("li");
+               creatLiPerson.innerHTML = personName;
+               let personLiinput = names.appendChild(creatLiPerson);
+               personNameInput.value = "";
+               warning.style.display = "none";
+               random.push(personLiinput);
+               personNameInput.focus();
+               creatLiPerson.setAttribute("data-id", random.length - 1);
+               creatLiPerson.addEventListener("click", (e) => {
+                    const index = e.target.dataset.id;
+                    random.splice(index, 1);
+                    creatLiPerson.remove();
+               });
+          }
+     };
 
      addtBtn.addEventListener("click", addPersonToList);
      startBtn.addEventListener("click", () => {
